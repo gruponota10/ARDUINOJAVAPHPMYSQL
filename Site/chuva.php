59b5b841312cf9087860396e8333bd38d74d948c
gruponota10 <38632944+gruponota10@users.noreply.github.com>
2018-05-23 08:07:35 -0300

    Update chuva.php

--- Site/chuva.php
+++ Site/chuva.php
@@ -6,10 +6,10 @@ $username = "root";
 $password = "newrootpassword";
 $dbname = "arduino";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password,$dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -20,7 +20,7 @@ $sql = "SELECT valortemp, valorhumi, valorchuva FROM dados WHERE iddados=1";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // output data of each row
+    
     while($row = $result->fetch_assoc()) {
         echo $row["valorchuva"];
     }
